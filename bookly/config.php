33ae886c3e3edc3d1ecd35c1e55e976732237f13
Akshay Goswami <154719447+Akshaygsm@users.noreply.github.com>
2024-12-04 10:22:37 +0530

    Add files via upload

---
+++ bookly/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password  = "";
+$database = "shop_db";
+
+$conn = mysqli_connect($server , $user , $password , $database);
+
+// if(!$conn){
+//     die("connection was failed ");
+// } else {
+//     echo "connection successfully";
+// }
+
+?>
